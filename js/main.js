6d2e45d6663b5c7b03ad352536ce3688e5b771c6
shenaan <shenaan@gmail.com>
2019-06-28 18:26:05 +0300

    homepage update

--- js/main.js
+++ js/main.js
@@ -8,24 +8,61 @@ $(document).ready(function () {
         }
     }
 
-    $('.header__menu-link').on('click', function (e) {
-        e.preventDefault();
-        $('body, html').toggleClass('no-scroll-initial');
-        $('.page-menu').toggleClass('is-active');
-    });
-
-    //reseting header classes on resize
+    //reseting header
 
     function headerReset() {
         $('.page-menu').removeClass('is-active');
+        $('.hamburger').removeClass('is-active');
         $('body, html').removeClass('no-scroll-initial');
     }
+
+    $('.header__menu-link').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('body, html').toggleClass('no-scroll-initial');
+        $('.page-menu').toggleClass('is-active');
+        $('.hamburger').toggleClass('is-active');
+    });
+
+    $(document).on('click', function (e) {
+        if (!e) e = window.event;
+        if ($('.page-menu').hasClass('is-active')) {
+            if (!$(e.target).closest('.page-menu.is-active').length) {
+              headerReset();
+            }
+        }
+    });
 
     // menu image replacement
 
     $('.page-nav__list-link').on('click', headerReset);
 
 
+    //homepage instagram slider
+    $('.homepage-follow__slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
 
     $(window).resize(function () {
         headerReset();
